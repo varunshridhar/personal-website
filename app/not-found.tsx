1f4bcf104ecca71dad1c3ae356494b5f83377a08
Varun Shridhar <varun.shridhar10@gmail.com>
2024-06-17 19:23:00 +0530

    Added basic site with 3 pages

---
+++ app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Hello, not found</h1>
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+      <><h1>Hello, Next.js!</h1><Link href="/technical-projects/music-platform-syncer">Dashboard</Link></>
+  )
+}
